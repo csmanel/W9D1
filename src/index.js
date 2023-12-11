5da9e19cedede6baeac836e1a094db9bb4e9407f
csmanel <csmanel@gmail.com>
2023-12-11 16:50:50 -0500

    MO!!!!!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-import MovingObject from "./moving-object.js";
+//entry
+import MovingObject from './moving-object.js';
 
 window.MovingObject = MovingObject;
-console.log("Webpack is working!")
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvasEle = document.getElementById('game-canvas');
+  canvasEle.width = 500;
+  canvasEle.height = 500;
+
+  const canvasCtx = canvasEle.getContext('2d');
+
+  const mo = new MovingObject({
+    pos: [250, 250],
+    vel: [10, 10],
+    radius: 100,
+    color: '#223',
+  });
+
+  // window.mo = mo;
+  mo.draw(canvasCtx);
+  // mo.move();
+});
--- src/moving-object.js
+++ src/moving-object.js
@@ -1,20 +1,25 @@
 class MovingObject {
   constructor(params) {
-    for (let key in params) {
-      this.key = params.key
-    }
-    // this.pos = params.pos;
-    // this.vel = params.vel;
-    // this.radius = params.vel
-    // this.color = params.color
+    this.pos = params.pos;
+    this.vel = params.vel;
+    this.radius = params.radius;
+    this.color = params.color;
   }
 
   draw(ctx) {
     ctx.beginPath();
-    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, Math.PI * 2, true)
-    ctx.fillStyle = this.color; 
-    ctx.fill()
+    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+    // ctx.stroke();
+    // ctx.closePath();
+    console.log('ive been drawn');
   }
+
+  // move() {
+  //   this.pos += this.vel;
+  //   console.log('i have been moved!!!!!!!!!');
+  // }
 }
 
 export default MovingObject;
